7029495c3732bffcdc966ceb52ccb1da1df34159
dimalakh <dmytrolakh@gmail.com>
2016-12-07 11:31:06 +0200

    sidebar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
+import { Ng2PaginationModule } from 'ng2-pagination';
+import { NgSemanticModule } from "ng-semantic";
+
 
 import { AppComponent } from './app.component';
 import { ReaderComponent } from './reader/reader.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { routing } from './app.routes';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReaderComponent,
-    CatalogComponent
+    CatalogComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     routing,
+    Ng2PaginationModule,
+    NgSemanticModule,
     MaterialModule.forRoot()
   ],
   providers: [],
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-catalog',
+  selector: 'catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.sass']
 })
 export class CatalogComponent implements OnInit {
   books: string[];
   constructor() {
-    this.books = ['1', '1', '1', '1', '1', '1', '1', '1'];
+    this.books = ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'];
    }
 
   ngOnInit() {
