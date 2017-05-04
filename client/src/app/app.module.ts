c81135468b349ca1df0a2643a616d751ec8c845c
dimalakh <dmytrolakh@gmail.com>
2017-05-04 19:11:44 +0300

    integrated angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,19 @@
-//Library imports
-import {NgModule} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
-import {RouterModule, Routes} from '@angular/router'
+import 'hammerjs';
+import { NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component'
-import { RootModule } from './root/root.module'
+import { AppComponent } from './app.component';
+import { RootModule } from './root/root.module';
 
 @NgModule({
     imports:[
         BrowserModule,
-        RootModule
+        RootModule,
+        MaterialModule.forRoot(),
+        BrowserAnimationsModule
     ],
     declarations: [
         AppComponent
